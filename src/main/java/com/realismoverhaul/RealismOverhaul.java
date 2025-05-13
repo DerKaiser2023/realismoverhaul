68d8272b86c84a609de9699faaf3878e2e6fec33
DerKaiser2023 <discordballs872@gmail.com>
2025-05-13 12:43:43 +0000

    part 1 of ruining this with realistic blood and damage

--- src/main/java/com/realismoverhaul/RealismOverhaul.java
+++ src/main/java/com/realismoverhaul/RealismOverhaul.java
@@ -1,8 +1,13 @@
 package com.realismoverhaul;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import com.realismoverhaul.core.DamageHandler;
+import com.realismoverhaul.proxy.CommonProxy;
+import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = RealismOverhaul.MODID, name = RealismOverhaul.NAME, version = RealismOverhaul.VERSION)
 public class RealismOverhaul {
@@ -10,13 +15,19 @@ public class RealismOverhaul {
     public static final String NAME = "Realism Overhaul";
     public static final String VERSION = "1.0";
 
-    @Mod.EventHandler
+    @SidedProxy(clientSide = "com.realismoverhaul.proxy.ClientProxy", serverSide = "com.realismoverhaul.proxy.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.Instance
+    public static RealismOverhaul instance;
+
+    @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        // Load config, register physics handlers
+        proxy.registerRenderes(); // Register client renderers (models, particles, etc)
     }
 
-    @Mod.EventHandler
+    @EventHandler
     public void init(FMLInitializationEvent event) {
-        // Register events, world gen, physics logic
+        MinecraftForge.EVENT_BUS.register(new DamageHandler()); // Register damage handling system
     }
 }
---
+++ src/main/java/com/realismoverhaul/capability/IBodyStatus.java
@@ -0,0 +1,6 @@
+package com.realisticdamage.realisticdamage.capability;
+public interface IBodyStatus {
+    float getHealth(String part);
+    void setHealth(String part, float value);
+}
+// Will edit later
